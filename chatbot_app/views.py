49d8b66311626af7b783348f2d8cc4951342bb51
Mahlaka <samimahlaka@gmail.com>
2025-05-27 20:09:11 -0500

    Removed OpenAI integration

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render
+
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from dotenv import load_dotenv
-import os
-
-# Create your views here.
-
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
 
 @api_view(['GET'])
 def chatbot_response(request):
     return Response({'response' : 'Hello, how can i help you?'})
 
+
 @api_view(['POST'])
 def chatbot_input(request):
     user_input = request.data.get('input')
-    return Response({'response' : f'you said {user_input}'})
+
+    
+    return Response({'response': ''})
+
 
 
 

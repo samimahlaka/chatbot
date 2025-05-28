40c0965e43e8d0007e4c4454efc7b501f7f6cb01
Mahlaka <samimahlaka@gmail.com>
2025-05-28 13:04:38 -0500

    Added JWT auth setup with SimpleJWT, ready to test superuser login

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
 from chatbot_app import views
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('chatbot_response/', views.chatbot_response, name='chatbot_response'),
     path('chatbot_input/', views.chatbot_input),
     path('chatbot_list/' , views.chatbot_list),
+    path('api/token/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/',TokenRefreshView.as_view(), name = 'token_refresh'),
 ]

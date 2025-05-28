8e883ea3319ebdc6eec26c8516a14312d0371147
Mahlaka <samimahlaka@gmail.com>
2025-05-28 11:35:26 -0500

    Added Conversation model, saving chats, and GET API for history

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -4,5 +4,6 @@ from chatbot_app import views
 
 urlpatterns = [
     path('chatbot_response/', views.chatbot_response, name='chatbot_response'),
-    path('chatbot_input/', views.chatbot_input)
+    path('chatbot_input/', views.chatbot_input),
+    path('chatbot_list/' , views.chatbot_list),
 ]
--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -2,7 +2,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
-
+from .models import Conversation
 @api_view(['GET'])
 def chatbot_response(request):
     return Response({'response' : 'Hello, how can i help you?'})
@@ -21,8 +21,17 @@ def chatbot_input(request):
         }
     )
     result = response.json()
-    return Response({'response': result['response']})
+    reply = result['response']
+    Conversation.objects.create(user_input =  user_input , response = reply )
+    return Response({'response': reply})
 
-
-
-
+@api_view(['GET'])
+def chatbot_list(request):
+    conversations = Conversation.objects.all()
+    data = []
+    for conversation in conversations:
+        data.append({'user-input' : conversation.user_input,
+                    'chatbot_response' : conversation.response})
+        
+    return Response(data)
+    
